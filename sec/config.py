c5e9053098e8076c34c698fc5361ec17c79be33b
HongChetra <58508518+HongChetra@users.noreply.github.com>
2020-04-29 12:03:10 +0700

    Added Postgresql

--- sec/config.py
+++ sec/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = '6b303869c00816089cfe22b007147d'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
