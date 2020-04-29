fe3d8e04ce59dfc76acb61b20032c711157eb8bf
HongChetra <58508518+HongChetra@users.noreply.github.com>
2020-04-29 12:20:41 +0700

    changed psycopg2

--- sec/config.py
+++ sec/config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = '6b303869c00816089cfe22b007147d'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
